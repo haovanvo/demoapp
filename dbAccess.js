96f4f7cfb558a6ef25eacb455e09f53f7d9d9a5d
haovanvo <vovanhaovn@gmail.com>
2017-10-09 19:51:21 +0700

    update db connectionstring

--- dbAccess.js
+++ dbAccess.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const url = 'mongodb://localhost:27017/demoappdb';
+const url = 'mongodb://admin:Abcd123!%40#@demoappdb-shard-00-00-gzx7l.mongodb.net:27017,demoappdb-shard-00-01-gzx7l.mongodb.net:27017,demoappdb-shard-00-02-gzx7l.mongodb.net:27017/test?ssl=true&replicaSet=demoappdb-shard-0&authSource=admin';
 
 var mongoClient = require('mongodb').MongoClient;
 
